b24e8c7481d7aab399e558e82cf93ff14864f575
Thiago <tgarcia@cpqi.com>
2020-07-20 15:53:32 -0300

    iniciando configurações

---
+++ src/main/java/com/github/thiagosousagarcia/sistemavendasspring/SistemaVendasSpringApplication.java
@@ -0,0 +1,13 @@
+package com.github.thiagosousagarcia.sistemavendasspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaVendasSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaVendasSpringApplication.class, args);
+	}
+
+}
