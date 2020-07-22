09678e5152c9e75024a00b2c2dffe563d93a72fe
Thiago <tsousa900@gmail.com>
2020-07-22 09:34:07 -0300

    ajustando relacionamento

--- src/main/java/com/github/thiagosousagarcia/sistemavendas/model/Venda.java
+++ src/main/java/com/github/thiagosousagarcia/sistemavendas/model/Venda.java
@@ -9,7 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -27,7 +27,7 @@ public class Venda {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@OneToOne
+	@ManyToOne
 	@JoinColumn(name = "CLIENTE_ID")
 	private Cliente cliente;
 	
